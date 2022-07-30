093bbba8168901ae12888290605f3fbbb100f3fb
Tran <ready0110@gmail.com>
2022-07-30 22:44:40 +0700

    QLSV update

--- QLSV/js/bai2/Validation.js
+++ QLSV/js/bai2/Validation.js
@@ -1,58 +1,117 @@
 function Validation() {
-    this.checkEmpty = function(inputVal, spanID, message) {
-        // inputVal khác rỗng
-        // trim() cắt các khoảng trắng
-        if (inputVal.trim() != '') {
-            // hợp lệ
-            document.getElementById(spanID).innerHTML = '';
-            document.getElementById(spanID).style.display = 'block';
-            return true;
-        }
-        // không hợp lệ
-        //điền câu thông báo lên UI
-        document.getElementById(spanID).innerHTML = message;
-        document.getElementById(spanID).style.display = 'none';
-        return false;
+  this.checkEmpty = function (inputVal, spanID, message) {
+    // inputVal khác rỗng
+    // trim() xoá khoảng trắng phía trước và sau
+    // "   ab " => "   ab ".trim() => "ab"
+    if (inputVal.trim() != "") {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
     }
+    // không hợp lệ
+    // điền câu thông báo lên UI
+    document.getElementById(spanID).innerHTML = message;
+    document.getElementById(spanID).style.display = "block";
+    return false;
+  };
+  this.checkID = function (inputVal, spanID, message, mangSV) {
+    /**
+     * Lấy từng phần tử của mangSV, để so sánh inputVal xem có trùng mã SV ko, nếu trùng thì thông báo lỗi, ko thì hợp lệ
+     */
+    // some() (ES6) => có chức năng duyệt mảng (chạy vòng lặp) (funct (item(sv), index){}) => kết quả trả về true/false
+    // giả sử mã chưa tồn tại (isExist = false)
+    var isExist = false;
+    isExist = mangSV.some(function (sv, index) {
+      // return kết quả của biểu thức so sánh
+      return sv.maSV === inputVal.replaceAll(" ", ""); // thay thế loại " " sang ""
+    });
 
-    this.checkID = function(inputVal, spanID, message, mangSV){
-        /**
-         * Duyệt mảng
-         *      so sánh mã của từng sv với inputVal
-         *      nếu trung => thông lỗi => return false
-         *      ngc lại => hợp lệ => return true
-         */
-
-        // some() => duyệt mảng, some(function(item(), index){}), return true/false
-        var isExist = false;
-        isExist = mangSV.some(function(sv, index){
-            // return kết quả của biểu thức so sánh
-            return sv.maSV === inputVal.replaceAll(' ','');
-        });
-
-        if(isExist) {
-            // mã bị trùng
-            document.getElementById(spanID).innerHTML = message;
-            document.getElementById(spanID).style.display = 'block';
-            return false;
-        } else {
-            document.getElementById(spanID).innerHTML = '';
-            document.getElementById(spanID).style.display = 'none';
-            return true;
-        }
+    if (isExist) {
+      // mã bị trùng => dữ liệu không hợp lệ
+      document.getElementById(spanID).innerHTML = message;
+      document.getElementById(spanID).style.display = "block";
+      return false;
+    } else {
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
     }
+  };
 
-    this.checkName = function(inputVal, spanID, message) {
-        var pattern = "^[a-z A-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$"
+  this.checkName = function (inputVal, spanID, message) {
+    var pattern =
+      /^[a-z  A-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$/;
 
-        if (inputVal.match(pattern)) {
-            // hợp lệ
-            document.getElementById(spanID).innerHTML = '';
-            document.getElementById(spanID).innerHTML = 'none';
-            return true;
-        }
-        document.getElementById(spanID).innerHTML = message;
-        document.getElementById(spanID).style.display = "block";
-        return false;
+    if (inputVal.match(pattern)) {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
+    } else {
+      document.getElementById(spanID).innerHTML = message;
+      document.getElementById(spanID).style.display = "block";
+      return false;
     }
+  };
+
+  this.checkEmail = function (inputVal, spanID, message) {
+    var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (inputVal.match(pattern)) {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
+    } else {
+      document.getElementById(spanID).innerHTML = message;
+      document.getElementById(spanID).style.display = "block";
+      return false;
+    }
+  };
+
+  this.checkPass = function (inputVal, spanID, message) {
+    var pattern =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,8}$/;
+
+    if (inputVal.match(pattern)) {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
+    } else {
+      document.getElementById(spanID).innerHTML = message;
+      document.getElementById(spanID).style.display = "block";
+      return false;
+    }
+  };
+
+  this.checkScore = function (inputVal, spanID, message) {
+    var pattern = /^(\d{1,2}(\.\d{1,2})?)$/;
+
+    if (inputVal.match(pattern) && inputVal <= 10) {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
+    } else {
+      document.getElementById(spanID).innerHTML = message;
+      document.getElementById(spanID).style.display = "block";
+      return false;
+    }
+  };
+
+  this.checkDropDown = function (selectID, spanID, message) {
+    // chọn option vị trí đầu tiên, là vị trí 0
+    var indexOption = document.getElementById(selectID).selectedIndex;
+    if (indexOption != 0) {
+      // hợp lệ
+      document.getElementById(spanID).innerHTML = "";
+      document.getElementById(spanID).style.display = "none";
+      return true;
+    }
+    document.getElementById(spanID).innerHTML = message;
+    document.getElementById(spanID).style.display = "block";
+    return false;
+  };
 }
--- QLSV/js/bai2/main.js
+++ QLSV/js/bai2/main.js
@@ -5,74 +5,128 @@
 // Global variable
 // => dssv thể hiện DanhSachSinhVien là biến toàn cục
 var dssv = new DanhSachSinhVien();
+var validation = new Validation();
 // Hàm rút gọn cú phát getElementByID
 function getELE(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 // localStorage để khi load lại browser dữ liệu k bị biến mất
 function setLocalStorage() {
-    // localStorage: đối tượng có sẵn của JS giúp thao tác về local storage của browsers
-    // JSON: đối tượng có sẵn của JS giúp chuỗi JSON
-    // dữ liệu lưu trữ ở localStorage là kiểu JSON
-    // chuyển từ Array => JSON
+  // localStorage: đối tượng có sẵn của JS giúp thao tác về local storage của browsers
+  // JSON: đối tượng có sẵn của JS giúp chuỗi JSON
+  // dữ liệu lưu trữ ở localStorage là kiểu JSON
+  // chuyển từ Array => JSON
 
-    // Khi mangSV thay đổi => gọi hàm setLocalStorage để cập nhật cho local
-    localStorage.setItem('DSSV', JSON.stringify(dssv.mangSV));
+  // Khi mangSV thay đổi => gọi hàm setLocalStorage để cập nhật cho local
+  localStorage.setItem("DSSV", JSON.stringify(dssv.mangSV));
 }
 
-
 function getLocalStorage() {
-    // getItem => trả về dữ liệu JSON
-    // JSON => Array
-    // localStorage chỉ lưu ở truyền đang chạy ứng dụng
-    // Kiểm tra xem browser có local Storage ko, ko có thì undefine, ko đc dùng như mảng
-    if (localStorage.getItem('DSSV') != undefined){
-        dssv.mangSV = JSON.parse(localStorage.getItem('DSSV'));
-    }
-    
-    hienThiDS(dssv.mangSV);
+  // getItem => trả về dữ liệu JSON
+  // JSON => Array
+  // localStorage chỉ lưu ở truyền đang chạy ứng dụng
+  // Kiểm tra xem browser có local Storage ko, ko có thì undefine, ko đc dùng như mảng
+  if (localStorage.getItem("DSSV") != undefined) {
+    dssv.mangSV = JSON.parse(localStorage.getItem("DSSV")); // parse() đổi json về mảng, JSON để backend có thể đọc
+  }
+
+  hienThiDS(dssv.mangSV);
 }
 getLocalStorage();
 // Thêm sinh viên
 function themSinhVien() {
+  var maSV = getELE("txtMaSV").value;
+  var tenSV = getELE("txtTenSV").value;
+  var email = getELE("txtEmail").value;
+  var password = getELE("txtPass").value;
+  var ngaySinh = getELE("txtNgaySinh").value;
+  var khoaHoc = getELE("khSV").value;
+  var toan = getELE("txtDiemToan").value;
+  var ly = getELE("txtDiemLy").value;
+  var hoa = getELE("txtDiemHoa").value;
+  //console.log(maSV, tenSV, email, password, ngaySinh, khoaHoc, toan, ly, hoa);
+
+  var isValid = true;
+  // Các bước kiểm tra dữ liệu
+  /**
+   * Issue: tên sv có value, mã sv ko có value => vẫn cho thêm sinh viên
+   * Expected: chỉ được thêm sv khi tất cả các dữ liệu đều hợp lệ
+   * Nếu có 1 dữ liệu không hợp lệ => thông báo + ko được thêm sv
+   * Root cause: do dấu = (gán) => chỉ giữ lại kt kết quả cuối cùng, các kết quả trước ghi đè mất
+   *
+   * Solution:
+   * C1: &&:so sánh true/false => checkMa && checkTen => false && true => false => khó đọc khi chỉnh sửa
+   * C2: tách các bước kiểm tra => dễ đọc code
+   * &: tính toán binary , true 1, false 0
+   * => checkMa & checkTen => 0 & 1 => 0
+   * isValid(cuoi cùng) = isValid & checkEmpty
+   * => isValid(cuối cùng) &= checkEmpty
+   */
+  isValid &= validation.checkEmpty(maSV,"spanMaSV","Mã SV không được để trống") && validation.checkID(maSV,"spanMaSV","Mã SV không được trùng", dssv.mangSV);
+
+  // tên sv (kiểm tra rỗng, kiểm tra ký tự chữ)
+  isValid &= validation.checkEmpty(
+    tenSV,
+    "spanTenSV",
+    "Tên SV không được để trống"
+  ) && validation.checkName(
+    tenSV,
+    "spanTenSV",
+    "Tên SV chỉ chứa ký tự chữ"
+  );
+
+  // Email SV (kiểm tra rỗng, kiểm tra format email)
+  isValid &= validation.checkEmpty(
+    email,
+    "spanEmailSV",
+    "Email không được để trống"
+  ) && validation.checkEmail(email, "spanEmailSV", "Email chưa đúng định dạng");
+
+  // Password (kiểm tra rỗng, kiểm tra format)
+  isValid &= validation.checkEmpty(
+    password,
+    "spanMatKhau",
+    "Mật khẩu không được để trống"
+  ) && validation.checkEmail(password, "spanMatKhau", "Mật khẩu chưa đúng định dạng");
+
+    // Điểm Toán (kiểm tra rỗng và định dạng số, điểm  lớn hơn 0, nhỏ hơn 10)
+    isValid &= validation.checkEmpty(
+        toan,
+        "spanToan",
+        "Điểm không được để trống"
+      ) && validation.checkEmail(toan, "spanToan", "Điểm chưa đúng định dạng");
+
+    // Điểm Lý (kiểm tra rỗng và định dạng số, điểm  lớn hơn 0, nhỏ hơn 10)
+    isValid &= validation.checkEmpty(
+        ly,
+        "spanLy",
+        "Điểm không được để trống"
+      ) && validation.checkEmail(ly, "spanLy", "Điểm chưa đúng định dạng");
+
+    // Điểm Hoá (kiểm tra rỗng và định dạng số, điểm  lớn hơn 0, nhỏ hơn 10)
+    isValid &= validation.checkEmpty(
+        hoa,
+        "spanHoa",
+        "Điểm không được để trống"
+      ) && validation.checkEmail(hoa, "spanHoa", "Điểm chưa đúng định dạng");
     
-    var maSV = getELE('txtMaSV').value;
-    var tenSV = getELE('txtTenSV').value;
-    var email = getELE('txtEmail').value;
-    var password = getELE('txtPass').value;
-    var ngaySinh = getELE('txtNgaySinh').value;
-    var khoaHoc = getELE('khSV').value;
-    var toan = getELE('txtDiemToan').value;
-    var ly = getELE('txtDiemLy').value;
-    var hoa = getELE('txtDiemHoa').value;
-    //console.log(maSV, tenSV, email, password, ngaySinh, khoaHoc, toan, ly, hoa);
-
-    var isValid = true;
-    // Các bước kiểm tra dữ liệu
-    /**
-     * Issue: tên sv có value, mã sv ko có value => vẫn cho thêm sinh viên
-     * Expected: chỉ được thêm sv khi tất cả các dữ liệu đều hợp lệ
-     * Nếu có 1 dữ liệu không hợp lệ => thông báo + ko được thêm sv
-     * Root cause: do dấu = (gán) => chỉ giữ lại kt kết quả cuối cùng, các kết quả trước ghi đè mất
-     * 
-     * Solution:
-     * C1: && => checkMa && checkTen => false && true => false => khó đọc khi chỉnh sửa
-     * C2: tách các bước kiểm tra => dễ đọc code
-     * &: tính toán binary , true 1, false 0
-     * => checkMa & checkTen => 0 & 1 => 0
-     * isValid(cuoi cùng) = isValid & checkEmpty
-     * => isValid(cuối cùng) &= checkEmpty
-     */
-    // Mã SV (kiểm tra rỗng, không được trùng)
-    isValid &= validation.checkEmpty(maSV,'spanMaSV', 'Mã sv không được để trống') && validation.checkID(maSV, 'spanMaSV', 'Mã SV ko được để trống');
-
-    // Tên SV (kiểm tra rỗng, kiểm tra ký tự chữ)
-    isValid &= validation.checkEmpty(tenSV, 'spanTenSV', 'Tên sv không được để trống');
-
-    if (isValid) {
-        // tạo thể hiện của SinhVien
-    var sv = new SinhVien(maSV, tenSV, email, password, ngaySinh, khoaHoc, Number(toan), Number(ly), Number(hoa));
+      // Khoá học (người dùng có chọn các lựa chọn khác cái đầu tiên không)
+      isValid &= validation.checkDropDown("khSV","spanKhoaHoc", "Khoá học chưa được chọn"); 
+  if (isValid) {
+    // tất cả dữ liệu hợp lệ
+    // tạo thể hiện của SinhVien
+    var sv = new SinhVien(
+      maSV,
+      tenSV,
+      email,
+      password,
+      ngaySinh,
+      khoaHoc,
+      Number(toan),
+      Number(ly),
+      Number(hoa)
+    );
     sv.tinhDTB();
     console.log(sv);
 
@@ -85,11 +139,9 @@ function themSinhVien() {
 
     setLocalStorage();
 
-    // thêm sv thì reset form
+    // thêm sv thì reset form để cái input đc trống
     resetForm();
-    }
-    
-    
+  }
 }
 
 /**
@@ -98,26 +150,26 @@ function themSinhVien() {
  * B2: Tạo hàng tr cho từng sv
  * B3: Tạo từng td, đưa các thuộc tính của sv đặt vào td
  * B4: Đem tất cả các thẻ tr hiển thị lên UI
- * 
+ *
  * Output: Các thẻ tr để hiển thị trên html
  * + 1 thẻ tr là 1 sv
  * + từng td là từng thuộc tính của sv
- *  
+ *
  */
 // Hiển thị danh sách sv
 function hienThiDS(mangSV) {
-    console.log('Mảng cần hiển thị ', mangSV);
-    // map(): hàm giúp duyệt mảng => lấy ra từng phần tử của mảng ( cú pháp ngắn gọn)
-    // ham1(ham2()) => callback function
-    // function(){} => hàm ẩn danh anonymous function
-    // map(phần tử của mảng, vị trí của phần tử)
-    // Sau khi duyệt mảng => content = "<tr></tr><tr></tr><tr></tr>"
-    var content = '';  // Giá trị ban đầu
-    mangSV.map(function(sv, index){
-        console.log(sv);
-        // string template (template literal)
-        // ${}: truyền biến vào cho chuỗi string template
-        var trELE = `
+  //console.log("Mảng cần hiển thị ", mangSV);
+  // map(): hàm giúp duyệt mảng => lấy ra từng phần tử của mảng ( cú pháp ngắn gọn)
+  // ham1(ham2()) => callback function
+  // function(){} => hàm ẩn danh anonymous function
+  // map(phần tử của mảng, vị trí của phần tử)
+  // Sau khi duyệt mảng => content = "<tr></tr><tr></tr><tr></tr>"
+  var content = ""; // Giá trị ban đầu
+  mangSV.map(function (sv, index) {
+    //console.log(sv);
+    // string template (template literal)
+    // ${}: truyền biến vào cho chuỗi string template
+    var trELE = `
             <tr>
                 <td>${sv.maSV}</td>
                 <td>${sv.tenSV}</td>
@@ -131,29 +183,28 @@ function hienThiDS(mangSV) {
                 </td>
             </tr>
         `;
-        //console.log(trELE);
-        // content (sv1 + sv2) = content(sv1) + "<tr>sv2</tr>"
-        content += trELE;
-        
-    });
+    //console.log(trELE);
+    // content (sv1 + sv2) = content(sv1) + "<tr>sv2</tr>"
+    content += trELE;
+  });
 
-   //console.log(content);
-    getELE('tbodySinhVien').innerHTML = content;
+  //console.log(content);
+  getELE("tbodySinhVien").innerHTML = content;
 }
 
 /**
  * Xóa sv => xóa phần tử khỏi mảng
  * => tìm được vị trí (index) của phần tử cần xóa
  * => dựa vào maSV => để tìm kiếm sv
- * 
- * 
+ *
+ *
  */
 
 function xoaSinhVien(ma) {
-    console.log(ma);
-    dssv.xoaSV(ma);
-    hienThiDS(dssv.mangSV);
-    setLocalStorage(dssv.mangSV);
+  //console.log(ma);
+  dssv.xoaSV(ma);
+  hienThiDS(dssv.mangSV);
+  setLocalStorage(dssv.mangSV);
 }
 
 /**
@@ -169,54 +220,64 @@ function xoaSinhVien(ma) {
  * => tìm vị trí sv cần cập nhật => gán giá trị sv vào vị trí tìm thấy
  */
 function xemChiTiet(ma) {
-    console.log('xem',ma);
-    var viTri = dssv.timViTri(ma);
-    if(viTri > -1){
-        // tìm thấy
-        var svTim = dssv.mangSV[viTri];
-        console.log(svTim);
-        getELE('txtMaSV').value = svTim.maSV;
-        getELE('txtMaSV').disabled = true;
+  //console.log("xem", ma);
+  var viTri = dssv.timViTri(ma);
+  if (viTri > -1) {
+    // tìm thấy
+    var svTim = dssv.mangSV[viTri];
+   // console.log(svTim);
+    getELE("txtMaSV").value = svTim.maSV;
+    getELE("txtMaSV").disabled = true;
 
-        getELE('txtTenSV').value = svTim.tenSV;
-        getELE('txtEmail').value = svTim.email;
-        getELE('txtPass').value = svTim.password;
-        getELE('txtNgaySinh').value = svTim.ngaySinh;
-        getELE('khSV').value = svTim.khoaHoc;
-        getELE('txtDiemToan').value = svTim.toan;
-        getELE('txtDiemLy').value = svTim.ly;
-        getELE('txtDiemHoa').value = svTim.hoa;
-
-    }
+    getELE("txtTenSV").value = svTim.tenSV;
+    getELE("txtEmail").value = svTim.email;
+    getELE("txtPass").value = svTim.password;
+    getELE("txtNgaySinh").value = svTim.ngaySinh;
+    getELE("khSV").value = svTim.khoaHoc;
+    getELE("txtDiemToan").value = svTim.toan;
+    getELE("txtDiemLy").value = svTim.ly;
+    getELE("txtDiemHoa").value = svTim.hoa;
+  }
 }
 
 function capNhatSinhVien(params) {
-    var maSV = getELE('txtMaSV').value;
-    var tenSV = getELE('txtTenSV').value;
-    var email = getELE('txtEmail').value;
-    var password = getELE('txtPass').value;
-    var ngaySinh = getELE('txtNgaySinh').value;
-    var khoaHoc = getELE('khSV').value;
-    var toan = getELE('txtDiemToan').value;
-    var ly = getELE('txtDiemLy').value;
-    var hoa = getELE('txtDiemHoa').value;
-    console.log(maSV, tenSV, email, password, ngaySinh, khoaHoc, toan, ly, hoa);
+  var maSV = getELE("txtMaSV").value;
+  var tenSV = getELE("txtTenSV").value;
+  var email = getELE("txtEmail").value;
+  var password = getELE("txtPass").value;
+  var ngaySinh = getELE("txtNgaySinh").value;
+  var khoaHoc = getELE("khSV").value;
+  var toan = getELE("txtDiemToan").value;
+  var ly = getELE("txtDiemLy").value;
+  var hoa = getELE("txtDiemHoa").value;
+  //console.log(maSV, tenSV, email, password, ngaySinh, khoaHoc, toan, ly, hoa);
 
-    // tạo thể hiện của SinhVien
-    var sv = new SinhVien(maSV, tenSV, email, password, ngaySinh, khoaHoc, Number(toan), Number(ly), Number(hoa));
-    sv.tinhDTB();
-    console.log(sv);
+  // tạo thể hiện của SinhVien
+  var sv = new SinhVien(
+    maSV,
+    tenSV,
+    email,
+    password,
+    ngaySinh,
+    khoaHoc,
+    Number(toan),
+    Number(ly),
+    Number(hoa)
+  );
+  sv.tinhDTB();
+  //console.log(sv);
 
-    dssv.capNhatSV(sv);
-    hienThiDS(dssv.mangSV);
-    setLocalStorage();
+  dssv.capNhatSV(sv);
+  hienThiDS(dssv.mangSV);
+  setLocalStorage();
 
-    resetForm();
+  resetForm(); // cập nhật 1 sv mới xong thì clear cái form
 }
 
 function resetForm() {
-    // chỉ dùng với thẻ form => clear các giá trị
-    getELE('formQLSV').reset();
+  // chỉ dùng với thẻ form => clear các giá trị ở trên form , giúp user nhập info mới
+  getELE("formQLSV").reset();
 
-    getELE('txtMaSV').disabled = false;
+  // xoá song mã SV vẫn ko sửa được ,nên phải tắt chế độ disbaled
+  getELE("txtMaSV").disabled = false;
 }
